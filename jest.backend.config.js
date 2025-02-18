95b208246e9ca970a697de4b492be79fc4e0150b
AndrewOng2066 <e0959899@u.nus.edu>
2025-02-18 19:04:55 +0800

    Fix the jest.mock issue

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   // display name
   displayName: "backend",
 
--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   // name displayed during tests
   displayName: "frontend",
 
@@ -20,11 +20,11 @@ export default {
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 
   // only run these tests
-  testMatch: ["<rootDir>/client/src/pages/Auth/*.test.js"],
+  testMatch: ["<rootDir>/client/src/pages/**/*.test.js"],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/Auth/**"],
+  collectCoverageFrom: ["client/src/pages/**/**"],
   coverageThreshold: {
     global: {
       lines: 100,
