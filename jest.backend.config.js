b2c151962dc6dcc8d4592177499cb1f62ca5775c
AndrewOng2066 <e0959899@u.nus.edu>
2025-02-18 19:26:02 +0800

    Revert "Fix the jest.mock issue"
    This reverts commit 95b208246e9ca970a697de4b492be79fc4e0150b.

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   // display name
   displayName: "backend",
 
--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   // name displayed during tests
   displayName: "frontend",
 
@@ -20,11 +20,11 @@ module.exports = {
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 
   // only run these tests
-  testMatch: ["<rootDir>/client/src/pages/**/*.test.js"],
+  testMatch: ["<rootDir>/client/src/pages/Auth/*.test.js"],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/**/**"],
+  collectCoverageFrom: ["client/src/pages/Auth/**"],
   coverageThreshold: {
     global: {
       lines: 100,
