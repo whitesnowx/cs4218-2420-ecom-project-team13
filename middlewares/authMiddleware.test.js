de1c10c4e1f416598e1c65a8fccbb209d9639e1c
tsulim <limxiujia18@gmail.com>
2025-02-21 01:57:51 +0800

    Write unit test for `authMiddleware` - `isAdmin`

--- middlewares/authMiddleware.test.js
+++ middlewares/authMiddleware.test.js
@@ -1,6 +1,6 @@
 import JWT from "jsonwebtoken";
 import userModel from "../models/userModel";
-import { requireSignIn } from "./authMiddleware";
+import { isAdmin, requireSignIn } from "./authMiddleware";
 
 jest.mock("jsonwebtoken");
 jest.mock("../models/userModel");
@@ -42,7 +42,7 @@ describe("Require Sign In Test", () => {
         const mockError = new Error("mock error");
         JWT.verify.mockImplementationOnce(() => {
             throw mockError;
-          });
+        });
         console.log = jest.fn();
 
         // Act
@@ -55,5 +55,74 @@ describe("Require Sign In Test", () => {
         );
         expect(next).not.toHaveBeenCalled();
         expect(console.log).toHaveBeenCalledWith(mockError);
+    });
+});
+
+describe("Is Admin Test", () => {
+    let req, res, next;
+    const user = {
+        _id: "123",
+        role: 0
+    }
+
+    beforeEach(() => {
+        req = {
+            user: {
+                _id: user._id
+            }
+        }
+        res = {
+            status: jest.fn().mockReturnThis(),
+            send: jest.fn()
+        };
+        next = jest.fn();
+    });
+
+    it("should return an error if user role is not 1", async () => {
+        // Arrange
+        userModel.findById = jest.fn().mockResolvedValue(user);
+
+        // Act
+        await isAdmin(req, res, next);
+
+        // Assert
+        expect(res.status).toHaveBeenCalledWith(401);
+        expect(res.send).toHaveBeenCalledWith({
+            success: false,
+            message: "Unauthorized Access"
+        });
+        expect(next).not.toHaveBeenCalled();
+    });
+
+    it("should call next() if user role is 1", async () => {
+        // Arrange
+        user.role = 1;
+        userModel.findById = jest.fn().mockResolvedValue(user);
+
+        // Act
+        await isAdmin(req, res, next);
+
+        // Assert
+        expect(next).toHaveBeenCalled();
+    });
+
+    it("should return an error", async () => {
+        // Arrange
+        const mockError = new Error("mock error");
+        userModel.findById = jest.fn().mockRejectedValue(mockError);
+        console.log = jest.fn();
+
+        // Act
+        await isAdmin(req, res, next);
+
+        // Assert
+        expect(next).not.toHaveBeenCalled();
+        expect(console.log).toHaveBeenCalledWith(mockError);
+        expect(res.status).toHaveBeenCalledWith(401);
+        expect(res.send).toHaveBeenCalledWith({
+            success: false,
+            error: mockError,
+            message: "Error in admin middleware"
+        });
     });
 });
