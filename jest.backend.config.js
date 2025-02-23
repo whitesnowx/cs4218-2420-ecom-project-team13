eecb9a602c6713a6507026f8c26f6ab3416703d0
whitesnowx <whitesnowx88@gmail.com>
2025-02-24 02:00:54 +0800

    configs

---
+++ jest.backend.config.js
@@ -0,0 +1,22 @@
+export default {
+  // display name
+  displayName: "backend",
+
+  // when testing backend
+  testEnvironment: "node",
+
+  transform: {},
+
+  // which test to run
+  testMatch: ["<rootDir>/controllers/*.test.js"],
+
+  // jest code coverage
+  collectCoverage: true,
+  collectCoverageFrom: ["controllers/**"],
+  coverageThreshold: {
+    global: {
+      lines: 100,
+      functions: 100,
+    },
+  },
+};
