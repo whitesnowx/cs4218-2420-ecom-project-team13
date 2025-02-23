7ca43e5a61ac2e7339978df8c2a4256daddc8f68
xuelinglow <xuelinglow09@gmail.com>
2025-02-24 03:58:51 +0800

    Add Profile Unit Test

--- client/src/pages/user/Orders.test.js
+++ client/src/pages/user/Orders.test.js
@@ -4,57 +4,120 @@ import { MemoryRouter, Routes, Route } from "react-router-dom";
 import Orders from "./Orders";
 import { useAuth } from "../../context/auth";
 import "@testing-library/jest-dom/extend-expect";
-import axios from 'axios';
+import axios from "axios";
 import moment from "moment";
 
-jest.mock('axios');
+jest.mock("axios");
 
 jest.mock("../../context/auth", () => ({
   useAuth: jest.fn(),
 }));
 
-  jest.mock('../../context/cart', () => ({
-    useCart: jest.fn(() => [null, jest.fn()]) 
-  }));
+jest.mock("../../context/cart", () => ({
+  useCart: jest.fn(() => [null, jest.fn()]),
+}));
 
-  jest.mock('../../context/search', () => ({
-    useSearch: jest.fn(() => [{ keyword: '' }, jest.fn()]) 
-  }));  
-
+jest.mock("../../context/search", () => ({
+  useSearch: jest.fn(() => [{ keyword: "" }, jest.fn()]),
+}));
 
 describe("Orders Component", () => {
-    const mockOrders = [
+  const mockOrders = [
+    {
+      id: "1",
+      status: "Completed",
+      buyer: { name: "John Doe" },
+      createAt: new Date("2025-02-17T12:00:00Z"),
+      payment: { success: true },
+      products: [
         {
-          id : "1",
-          status: "Completed",
-          buyer: { name: "John Doe" },
-          createAt: new Date("2025-02-17T12:00:00Z"),
-          payment: { success: true },
-          products: [
-            { id: "p1", name: "Product 1", description: "Description 1", price: 250 },
-          ],
-        }
-      ];
-    describe("Orders Component", () => {
-        beforeEach(() => {
-            jest.clearAllMocks();
-            useAuth.mockReturnValue([{ token: "mockToken" }, jest.fn()]);
-            axios.get.mockResolvedValue({ data: mockOrders });
-        });
-        it("renders order information", async () => {
-            render(
-              <MemoryRouter>
-                <Orders />
-              </MemoryRouter>
-            );
-            expect(screen.getByText("All Orders")).toBeInTheDocument();
-            expect(await screen.findByText(mockOrders[0].status)).toBeInTheDocument();
-            expect(await screen.findByText(mockOrders[0].buyer.name)).toBeInTheDocument();
-            expect(await screen.findByText(moment(mockOrders[0].createAt).fromNow())).toBeInTheDocument();
-            expect(await screen.findByText(mockOrders[0].payment.success ? "Success" : "Failed")).toBeInTheDocument();
-            expect(await screen.findByTestId("product-count")).toHaveTextContent(mockOrders[0].products.length);
-
-        });
-        //write test to handle error when fetching or render empty
+          id: "p1",
+          name: "Product 1",
+          description: "Description 1",
+          price: 250,
+        },
+      ],
+    },
+  ];
+  describe("Orders Component", () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+      useAuth.mockReturnValue([{ token: "mockToken" }, jest.fn()]);
+      axios.get.mockResolvedValue({ data: mockOrders });
     });
+
+    it("renders order information", async () => {
+      render(
+        <MemoryRouter>
+          <Orders />
+        </MemoryRouter>
+      );
+      await waitFor(() => {
+        expect(axios.get).toHaveBeenCalledWith('/api/v1/auth/orders'); 
+      });
+      expect(screen.getByText("All Orders")).toBeInTheDocument();
+      
+      expect(await screen.findByText(mockOrders[0].status)).toBeInTheDocument();
+      expect(
+        await screen.findByText(mockOrders[0].buyer.name)
+      ).toBeInTheDocument();
+      expect(
+        await screen.findByText(moment(mockOrders[0].createAt).fromNow())
+      ).toBeInTheDocument();
+      expect(
+        await screen.findByText(
+          mockOrders[0].payment.success ? "Success" : "Failed"
+        )
+      ).toBeInTheDocument();
+      expect(await screen.findByTestId("product-count")).toHaveTextContent(
+        mockOrders[0].products.length
+      );
+    });
+
+    it("should display error message on failure", async () => {
+      const consoleSpy = jest.spyOn(console, "log");
+      axios.get.mockRejectedValue(new Error("Error fetching orders"));
+
+      render(
+        <MemoryRouter>
+          <Orders />
+        </MemoryRouter>
+      );
+
+      await waitFor(() => {
+        expect(consoleSpy).toHaveBeenCalledWith(expect.any(Error)); 
+      });
+      consoleSpy.mockRestore(); 
+    });
+
+    it("should be able to process orders with payment failure", async () => {
+      const mockOrdersFailure = [
+        {
+          ...mockOrders[0],
+          payment: { success: false },
+        },
+      ];
+      axios.get.mockResolvedValue({ data: mockOrdersFailure });
+      render(
+        <MemoryRouter>
+          <Orders />
+        </MemoryRouter>
+      );
+      expect(screen.getByText("All Orders")).toBeInTheDocument();
+      expect(await screen.findByText("Failed")).toBeInTheDocument();
+    });
+
+    it("should not render if auth token is not true", async () => {
+      useAuth.mockReturnValue([{ token: undefined }, jest.fn()]);
+      render(
+        <MemoryRouter>
+          <Orders />
+        </MemoryRouter>
+      );
+      await waitFor(() => {
+        expect(axios.get).not.toHaveBeenCalledWith('/api/v1/auth/orders'); 
+      });
+      expect(await screen.queryByText(mockOrders[0].buyer.name)).not.toBeInTheDocument();
+    })
+  });
 });
