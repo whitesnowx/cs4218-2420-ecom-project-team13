08ec1c5a9463181618c63acfe50b373bb8418c74
whitesnowx <whitesnowx88@gmail.com>
2025-02-24 19:59:14 +0800

    productController.test

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -8,7 +8,8 @@ export default {
   transform: {},
 
   // which test to run
-  testMatch: ["<rootDir>/controllers/*.test.js"],
+  // testMatch: ["<rootDir>/controllers/*.test.js"],
+  testMatch: ["<rootDir>/controllers/productController.test.js"],
 
   // jest code coverage
   collectCoverage: true,
