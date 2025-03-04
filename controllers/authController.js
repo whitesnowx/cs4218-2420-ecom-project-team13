01113c85372bf6bb0cc660bbb3cc1d05d9343cd2
tsulim <limxiujia18@gmail.com>
2025-03-05 03:11:41 +0800

    Fix bugs in `authController`

--- controllers/authController.js
+++ controllers/authController.js
@@ -172,7 +172,7 @@ export const updateProfileController = async (req, res) => {
     const { name, email, password, address, phone } = req.body;
     const user = await userModel.findById(req.user._id);
     //password
-    if (!password || (password && password.length < 6)) {
+    if (password && password.length < 6) {
       return res.status(400).send({
         error: "Password is required and 6 character long"
       });
