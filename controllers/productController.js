b0fa35a8d83c744c583370e2281a2c71455d3503
AndrewOng2066 <e0959899@u.nus.edu>
2025-03-07 18:07:22 +0800

    Split the photo's checking to individual checking of photot and photo's size in createProductController function

--- controllers/productController.js
+++ controllers/productController.js
@@ -34,10 +34,14 @@ export const createProductController = async (req, res) => {
         return res.status(500).send({ error: "Category is Required" });
       case !quantity:
         return res.status(500).send({ error: "Quantity is Required" });
-      case photo && photo.size > 1000000:
+      case !photo:
         return res
           .status(500)
-          .send({ error: "photo is Required and should be less then 1mb" });
+          .send({ error: "Photo is Required" });
+      case photo && photo.size > 1000000:
+        return res
+            .status(500)
+            .send({ error: "Photo should be less then 1mb" });
     }
 
     const products = new productModel({ ...req.fields, slug: slugify(name) });
